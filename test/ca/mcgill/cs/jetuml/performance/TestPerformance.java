7bb2e3d19d545da724d395fc53876c958797407f
delphinepilon <delphine.pilon@icloud.com>
2021-10-22 11:25:11 -0400

    Fix the performance tests.

--- test/ca/mcgill/cs/jetuml/performance/TestPerformance.java
+++ test/ca/mcgill/cs/jetuml/performance/TestPerformance.java
@@ -1,137 +1,59 @@
 package ca.mcgill.cs.jetuml.performance;
 
-import java.time.Duration;
-import java.time.Instant;
+ import java.io.IOException;
+ import java.nio.file.Path;
+ import java.time.Duration;
+ import java.time.Instant;
 
-import org.junit.jupiter.api.Test;
+ import ca.mcgill.cs.jetuml.diagram.Diagram;
+ import ca.mcgill.cs.jetuml.diagram.DiagramType;
+ import ca.mcgill.cs.jetuml.persistence.PersistenceService;
+ import javafx.scene.canvas.Canvas;
+ import javafx.scene.canvas.GraphicsContext;
 
-import ca.mcgill.cs.jetuml.diagram.nodes.ClassNode;
-import ca.mcgill.cs.jetuml.diagram.nodes.NoteNode;
-import ca.mcgill.cs.jetuml.diagram.nodes.ObjectNode;
-import ca.mcgill.cs.jetuml.diagram.nodes.PackageNode;
-import ca.mcgill.cs.jetuml.viewers.nodes.NodeViewerRegistry;
+ /**
+  * Tests the performance of drawing a diagram. 
+  */
+ public class TestPerformance 
+ {
+ 	private static final int NUMBER_OF_TRIALS = 10;
+ 	/**
+ 	 * Test method. 
+ 	 */
+ 	public static void main(String[] pArgs) throws Exception
+ 	{
+ 		testDrawDiagram(1);
+ 		testDrawDiagram(2);
+ 	}
 
-/**
- * Tests the performance of certain components of JetUML. 
- */
-public class TestPerformance 
-{
-	private static final int NUMBER_OF_ROUNDS = 10;
-	private static final int NUMBER_OF_ITERATIONS_PER_ROUND = 10000;
-	
-	public static void main(String[] pArgs)
-	{
-		testGetBoundsPerformanceWithTenNodesContinuouslyBeingTranslated();
-		testGetBoundsPerformanceWithThreeNodesContinuouslyBeingTranslated();
-		testGetBoundsPerformanceWithClassNodeContinuouslyBeingTranslated();
-		
-	}
-	
-	/**
-	 * Tests NodeViewerRegistry.getBounds(Node) as a ClassNode is continuously being translated. 
-	 */
-	public static void testGetBoundsPerformanceWithClassNodeContinuouslyBeingTranslated()
-	{
-		double averageTime = 0.0;
-		
-		for (int i = 0; i < NUMBER_OF_ROUNDS; i++)
-		{
-			ClassNode classNode = new ClassNode();
-			
-			Instant start = Instant.now();
-			for (int j = 0; j < NUMBER_OF_ITERATIONS_PER_ROUND; j++)
-			{
-				classNode.translate(i, i);
-				NodeViewerRegistry.getBounds(classNode);
-			}
-			Instant end = Instant.now();
-			
-			averageTime += Duration.between(start, end).getNano()*0.000001;
-		}
-		averageTime = averageTime / NUMBER_OF_ROUNDS;
-		System.out.println("(1 node) Average duration of " + NUMBER_OF_ROUNDS + " trials in milliseconds : " + averageTime);
-	}
-	
-	/**
-	 * Tests NodeViewerRegistry.getBounds(Node) as 3 nodes of different types keep being translated. 
-	 */
-	public static void testGetBoundsPerformanceWithThreeNodesContinuouslyBeingTranslated()
-	{
-		double averageTime = 0.0;
-		
-		for (int i = 0; i < NUMBER_OF_ROUNDS; i++)
-		{
-			PackageNode packageNode = new PackageNode();
-			ClassNode classNode = new ClassNode();
-			NoteNode noteNode = new NoteNode();
-			
-			Instant start = Instant.now();
-			for (int j = 0; j < NUMBER_OF_ITERATIONS_PER_ROUND; j++)
-			{
-				packageNode.translate(i, i);
-				classNode.translate(i, i);
-				noteNode.translate(i, i);
-				NodeViewerRegistry.getBounds(classNode);
-				NodeViewerRegistry.getBounds(packageNode);
-				NodeViewerRegistry.getBounds(noteNode);
-			}
-			Instant end = Instant.now();
-			
-			averageTime += Duration.between(start, end).getNano()*0.000001;
-		}
-		averageTime = averageTime / NUMBER_OF_ROUNDS;
-		System.out.println("(3 nodes) Average duration of " + NUMBER_OF_ROUNDS + " trials in milliseconds : " + averageTime);
-	}
-	
-	/**
-	 * Tests NodeViewerRegistry.getBounds(Node) as 10 nodes of different types keep being translated. 
-	 */
-	public static void testGetBoundsPerformanceWithTenNodesContinuouslyBeingTranslated()
-	{
-		double averageTime = 0.0;
-		
-		for (int i = 0; i < NUMBER_OF_ROUNDS; i++)
-		{
-			PackageNode packageNode = new PackageNode();
-			ClassNode classNode = new ClassNode();
-			NoteNode noteNode = new NoteNode();
-			ObjectNode objectNode1 = new ObjectNode();
-			ObjectNode objectNode2 = new ObjectNode();
-			ObjectNode objectNode3 = new ObjectNode();
-			ObjectNode objectNode4 = new ObjectNode();
-			ObjectNode objectNode5 = new ObjectNode();
-			ObjectNode objectNode6 = new ObjectNode();
-			ObjectNode objectNode7 = new ObjectNode();
-			
-			Instant start = Instant.now();
-			for (int j = 0; j < NUMBER_OF_ITERATIONS_PER_ROUND; j++)
-			{
-				packageNode.translate(i, i);
-				classNode.translate(i, i);
-				noteNode.translate(i, i);
-				objectNode1.translate(i, i);
-				objectNode2.translate(i, i);
-				objectNode3.translate(i, i);
-				objectNode4.translate(i, i);
-				objectNode5.translate(i, i);
-				objectNode6.translate(i, i);
-				objectNode7.translate(i, i);
-				NodeViewerRegistry.getBounds(classNode);
-				NodeViewerRegistry.getBounds(packageNode);
-				NodeViewerRegistry.getBounds(noteNode);
-				NodeViewerRegistry.getBounds(objectNode1);
-				NodeViewerRegistry.getBounds(objectNode2);
-				NodeViewerRegistry.getBounds(objectNode3);
-				NodeViewerRegistry.getBounds(objectNode4);
-				NodeViewerRegistry.getBounds(objectNode5);
-				NodeViewerRegistry.getBounds(objectNode6);
-				NodeViewerRegistry.getBounds(objectNode7);
-			}
-			Instant end = Instant.now();
-			
-			averageTime += Duration.between(start, end).getNano()*0.000001;
-		}
-		averageTime = averageTime / NUMBER_OF_ROUNDS;
-		System.out.println("(10 nodes) Average duration of " + NUMBER_OF_ROUNDS + " trials in milliseconds : " + averageTime);
-	}
-}
+ 	private static void testDrawDiagram(int pNumberOfDrawCalls) throws IOException 
+ 	{
+ 		Canvas canvas = new Canvas();
+ 		GraphicsContext graphicContext = canvas.getGraphicsContext2D();
+ 		Diagram diagram = PersistenceService.read(Path.of("testdata", "performanceDiagram.class.jet").toFile()).diagram();
+
+ 		double avgExecutionTime = 0.0;
+ 		for (int i = 0; i < NUMBER_OF_TRIALS+1; i++)
+ 		{
+ 			Instant start = Instant.now();
+ 			for (int j = 0; j < pNumberOfDrawCalls; j++)
+ 			{
+ 				DiagramType.viewerFor(diagram).draw(diagram, graphicContext);
+ 			}
+ 			Instant stop = Instant.now();
+
+ 			if (i == 0)
+ 			{
+ 				continue;
+ 			}
+ 			else
+ 			{
+ 				avgExecutionTime += Duration.between(start, stop).toMillis();
+ 			}
+ 		}
+ 		avgExecutionTime = avgExecutionTime / NUMBER_OF_TRIALS;
+
+ 		System.out.println("Call DiagramViewer.draw(diagram) " + pNumberOfDrawCalls + " time(s) : ");
+ 		System.out.println("Average Duration (ms) over " + NUMBER_OF_TRIALS + " trials : " + avgExecutionTime + "\n");
+ 	}
+ } 
